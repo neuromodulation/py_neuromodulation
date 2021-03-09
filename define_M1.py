eb3ca7f18002c65ba0e98e546cd25a5b2854252b
Richard Koehler <richard.koehler@outlook.de>
2021-03-09 11:47:09 +0100

    Rereference and define_M1 updates

--- define_M1.py
+++ define_M1.py
@@ -1,41 +1,96 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-mov_substr_list = ["TTL", "ANALOG", "MOV"]
-used_substr_list = ["ecog", "seeg"]
 
-def set_M1(ch_names, ch_type):
-    """
-    Return a default M1 dataframe that set's 
-    "name" to parameter ch_names 
-    "rereference" to "average" 
-    "used" for all "ECOG" and "SEEG" to 1 
-    "target" for all "TTL", "ANALOG" and "MOV" to 1 
+def set_M1(ch_names, ch_types, reference='default'):
+    """Return dataframe with channel-specific settings.
+
+    Return an M1 dataframe with the columns: "name", "rereference", "used",
+    "target", "ECOG"]. "name" is set to ch_names, "rereference" can be specified
+    individually. "used" is set to 1 for all channels containing "ECOG", "SEEG",
+    "LFP" or "DBS", else to 0. "target" is set to 1 for all channels containing
+    "TTL", "ANALOG", "MOV" and "ROTA", else to 0
 
     Args:
-        ch_names (list): channel names 
-        ch_type (list): channel types
+        ch_names (list):
+            list of channel names. Should each contain one of the keywords
+            (lower or upper case): "ECOG", "LFP", "DBS", "SEEG", "TTL",
+            "ANALOG", "ROTA" or "MOV".
+        ch_types (list):
+            list of channel types. Should optimally be of the types: "ECOG",
+            "DBS" or "SEEG". "LFP" is also accepted.
+        reference (string or list of strings):
+            re-referencing scheme. Default is "default". This sets ECOG channel
+            references to "average" and creates a bipolar referencing scheme
+            for LFP/DBS/SEEG channels, where each channel is referenced to
+            the adjacent lower channel, split by left and right hemisphere.
+            For this, the channel names must contain the substring "_L_" and/or
+            "_R_" (lower or upper case). CAVE: Adjacent channels will be
+            determined using the sorted() function.
+
     
     Returns: 
-        df in M1 format
+        df: dataframe in M1 format
     """
 
-    # set here: name, reference, used, target, ECoG 
-    df = pd.DataFrame(np.nan, index=np.arange(len(list(ch_names))), columns=['name', 'rereference', 'used', 'target'])
+    if not (len(ch_names) == len(ch_types)):
+        raise Exception("Sorry, no numbers below zero")
 
-    ch_used = [ch_idx for ch_idx, ch in enumerate(ch_type) if any(used_substr in ch for used_substr in used_substr_list)]
-    used = np.zeros(len(ch_names))
-    used[ch_used] = 1
-    df['used'] = used.astype(int)
+    mov_substrs = ["ttl", "analog", "mov", "rota"]
+    used_substrs = ["ecog", "seeg"]
+    lfp_types = ['seeg', 'dbs', 'lfp']
+
+    df = pd.DataFrame(np.nan, index=np.arange(len(list(ch_names))),
+                      columns=['name', 'rereference', 'used', 'target', 'ECOG'])
     df['name'] = ch_names
+    # set used column
+    df['used'] = [1 if any(used_substr.lower() in ch_name.lower()
+                           or used_substr.lower() in ch_type.lower()
+                           for used_substr in used_substrs)
+                  else 0 for ch_type, ch_name in zip(ch_types, ch_names)]
+    # set target column
+    df['target'] = [1 if any(mov_substr.lower() in ch_name.lower()
+                             for mov_substr in mov_substrs) else 0
+                    for ch_idx, ch_name in enumerate(ch_names)]
 
+    # set ECOG column
+    df['ECOG'] = [1 if 'ecog' in ch_name.lower() or 'ecog'
+                  in ch_types[ch_idx].lower()
+                  else 0 for ch_idx, ch_name in enumerate(ch_names)]
 
-    # check here for TTL, ANALOG 
-    ch_mov = [ch_idx for ch_idx, ch in enumerate(ch_names) if any(mov_substr in ch for mov_substr in mov_substr_list)]
-    target = np.zeros(len(ch_names))
-    target[ch_mov] = 1
-    df['target'] = target.astype(int)
-
-    df['rereference'] = ["average" for x in range(len(ch_names))]
-
+    if any(reference):
+        if reference in ['default', 'Default']:
+            ecog_chs = []
+            lfp_chs = []
+            other_chs = []
+            for ch_name, ch_type in zip(ch_names, ch_types):
+                if "ecog" in ch_type.lower() or "ecog" in ch_name.lower():
+                    ecog_chs.append(ch_name)
+                elif any(lfp_type.lower() in ch_type.lower()
+                         or lfp_type.lower() in ch_name.lower()
+                         for lfp_type in lfp_types):
+                    lfp_chs.append(ch_name)
+                else:
+                    other_chs.append(ch_name)
+            lfp_l = sorted(
+                [lfp_ch for lfp_ch in lfp_chs if '_l_' in lfp_ch.lower()])
+            lfp_r = sorted(
+                [lfp_ch for lfp_ch in lfp_chs if '_r_' in lfp_ch.lower()])
+            lfp_l_refs = [lfp_l[i - 1] if i > 0 else 'None' for i in
+                          range(len(lfp_l))]
+            lfp_r_refs = [lfp_r[i - 1] if i > 0 else 'None' for i in
+                          range(len(lfp_r))]
+            ref_idx = list(df.columns).index('rereference')
+            for ecog_ch in ecog_chs:
+                df.iloc[df[df['name'] == ecog_ch].index[0], ref_idx] = 'average'
+            for i, lfp in enumerate(lfp_l):
+                df.iloc[df[df['name'] == lfp].index[0], ref_idx] = lfp_l_refs[i]
+            for i, lfp in enumerate(lfp_r):
+                df.iloc[df[df['name'] == lfp].index[0], ref_idx] = lfp_r_refs[i]
+            for other_ch in other_chs:
+                df.iloc[df[df['name'] == other_ch].index[0], ref_idx] = 'None'
+        else:
+            df['rereference'] = reference
+    else:
+        df.loc[:, 'rereference'] = "None"
     return df
