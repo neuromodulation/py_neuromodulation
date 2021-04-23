8fafb246183746b2f0fd890042aee00787f547bd
timonmerk <timon.merk@charite.de>
2021-04-23 12:30:41 +0200

    adapt verbose

--- pyneuromodulation/start_BIDS.py
+++ pyneuromodulation/start_BIDS.py
@@ -52,7 +52,7 @@ def est_features_run(PATH_RUN, PATH_M1=None) -> None:
     # optionally reduce timing for faster test completion
     
     #LIMIT_LOW = 0
-    #LIMIT_HIGH = 5000
+    #LIMIT_HIGH = 10000
     #raw_arr_data = raw_arr_data[:, LIMIT_LOW:LIMIT_HIGH]
 
 
@@ -75,7 +75,7 @@ def est_features_run(PATH_RUN, PATH_M1=None) -> None:
 
     # initialize run object
     run_analysis_ = run_analysis.Run(features_, settings_wrapper.settings,
-                                     rereference_, projection_, resample_, verbose=False)
+                                     rereference_, projection_, resample_, verbose=True)
 
     while True:
         ieeg_batch = next(gen, None)
