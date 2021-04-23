17931fd810f052c00ded47cc8e37ad40c0fa6b7b
timonmerk <timon.merk95@gmail.com>
2021-04-23 11:39:37 +0200

    add relative example path write

--- examples/example_BIDS.py
+++ examples/example_BIDS.py
@@ -3,6 +3,7 @@ import sys
 from bids import BIDSLayout
 from itertools import product
 import os
+import json
 
 os.chdir(os.path.join(os.pardir,'pyneuromodulation'))
 sys.path.append(os.path.join(os.pardir,'pyneuromodulation'))
@@ -16,6 +17,17 @@ if __name__ == "__main__":
 
     BIDS_EXAMPLE_PATH = os.path.join(PATH_PYNEUROMODULATION, 'pyneuromodulation',
                                      'tests', 'data')
+
+    # write BIDS example path in settings.json
+    with open('settings.json', encoding='utf-8') as json_file:
+        settings = json.load(json_file)
+    settings["BIDS_path"] = BIDS_EXAMPLE_PATH
+
+    # write relative feature output folder
+    settings["out_path"] = os.path.join(PATH_PYNEUROMODULATION, 'pyneuromodulation',
+                                        'tests', 'data', 'derivatives')
+    with open('settings.json', 'w') as f:
+        json.dump(settings, f)
 
     PATH_RUN = os.path.join(BIDS_EXAMPLE_PATH, 'sub-testsub', 'ses-EphysMedOff',
                             'ieeg', "sub-testsub_ses-EphysMedOff_task-buttonpress_ieeg.vhdr")
