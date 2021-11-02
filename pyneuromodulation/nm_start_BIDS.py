9dc5e7f30cfe31c478055fc7a07f6958005c1190
timonmerk <timon.merk@charite.de>
2021-11-02 21:41:41 +0100

    reverse changes

--- pyneuromodulation/nm_start_BIDS.py
+++ pyneuromodulation/nm_start_BIDS.py
@@ -2,38 +2,19 @@ import os
 from pathlib import Path
 import mne
 
-from pyneuromodulation import (
-    nm_IO,
-    nm_projection,
-    nm_generator,
-    nm_rereference,
-    nm_run_analysis,
-    nm_features,
-    nm_resample,
-    nm_settings,
-)
+from pyneuromodulation import nm_IO, nm_projection, nm_generator, nm_rereference, \
+    nm_run_analysis, nm_features, nm_resample, nm_settings
 
 
 class NM_BIDS:
-    def __init__(
-        self,
-        PATH_RUN,
-        PATH_NM_CHANNELS=None,
-        PATH_SETTINGS=None,
-        PATH_ANNOTATIONS=None,
-        PATH_BIDS=None,
-        PATH_GRIDS=None,
-        PATH_OUT=None,
-        LIMIT_DATA=False,
-        LIMIT_LOW=0,
-        LIMIT_HIGH=25000,
-        ECOG_ONLY=False,
-        verbose=True,
-    ) -> None:
+
+    def __init__(self, PATH_RUN, PATH_NM_CHANNELS=None, PATH_SETTINGS=None,
+                 PATH_ANNOTATIONS=None, PATH_BIDS=None, PATH_GRIDS=None, PATH_OUT=None,
+                 LIMIT_DATA=False, LIMIT_LOW=0, LIMIT_HIGH=25000,
+                 ECOG_ONLY=False, verbose=True) -> None:
         """Start feature estimation by reading settings, creating or reading
         nm_channels.csv file with default rereference function (ECoG CAR; depth LFP bipolar)
         Parameters
-
         Parameters
         ----------
         PATH_RUN : string
@@ -80,19 +61,11 @@ class NM_BIDS:
         self.features = None
         self.run_analysis = None
 
-        self.set_settings(
-            self.PATH_SETTINGS, PATH_BIDS=PATH_BIDS, PATH_OUT=PATH_OUT
-        )
+        self.set_settings(self.PATH_SETTINGS, PATH_BIDS=PATH_BIDS, PATH_OUT=PATH_OUT)
 
         # read BIDS data
-        (
-            self.raw_arr,
-            self.raw_arr_data,
-            self.fs,
-            self.line_noise,
-        ) = nm_IO.read_BIDS_data(
-            self.PATH_RUN, self.settings_wrapper.settings["BIDS_path"]
-        )
+        self.raw_arr, self.raw_arr_data, self.fs, self.line_noise = nm_IO.read_BIDS_data(
+            self.PATH_RUN, self.settings_wrapper.settings['BIDS_path'])
 
         if self.PATH_ANNOTATIONS is not None:
             self.set_annotations(self.PATH_ANNOTATIONS)
@@ -117,15 +90,11 @@ class NM_BIDS:
     def set_settings(self, PATH_SETTINGS=None, PATH_BIDS=None, PATH_OUT=None):
         if PATH_SETTINGS is None:
             # if no path to nm_settings.json is given, take the deafult one in py_neuromodulation
-            PATH_SETTINGS = os.path.join(
-                os.path.dirname(nm_IO.__file__), "nm_settings.json"
-            )
+            PATH_SETTINGS = os.path.join(os.path.dirname(nm_IO.__file__), 'nm_settings.json')
             self.settings_wrapper = nm_settings.SettingsWrapper(PATH_SETTINGS)
         else:
             # PATH_SETTINGS is parametrized
-            self.settings_wrapper = nm_settings.SettingsWrapper(
-                settings_path=self.PATH_SETTINGS
-            )
+            self.settings_wrapper = nm_settings.SettingsWrapper(settings_path=self.PATH_SETTINGS)
         if PATH_BIDS is not None:
             self.settings_wrapper.settings["BIDS_path"] = PATH_BIDS
         if PATH_OUT is not None:
@@ -133,28 +102,15 @@ class NM_BIDS:
 
     def set_annotations(self):
         try:
-            self.annot = mne.read_annotations(
-                os.path.join(
-                    self.PATH_ANNOTATIONS,
-                    os.path.basename(self.PATH_RUN)[:-5] + ".txt",
-                )
-            )
+            self.annot = mne.read_annotations(os.path.join(self.PATH_ANNOTATIONS,
+                                              os.path.basename(self.PATH_RUN)[:-5]+".txt"))
             self.raw_arr.set_annotations(self.annot)
             # annotations starting with "BAD" are omitted with reject_by_annotations 'omit' param
-            self.raw_arr_data = self.raw_arr.get_data(
-                reject_by_annotation="omit"
-            )
+            self.raw_arr_data = self.raw_arr.get_data(reject_by_annotation='omit')
         except FileNotFoundError:
             print("Annotations file could not be found")
-            print(
-                "expected location: "
-                + str(
-                    os.path.join(
-                        self.PATH_ANNOTATIONS,
-                        os.path.basename(self.PATH_RUN)[:-5] + ".txt",
-                    )
-                )
-            )
+            print("expected location: "+str(os.path.join(self.PATH_ANNOTATIONS,
+                                            os.path.basename(self.PATH_RUN)[:-5]+".txt")))
             pass
 
     def set_projection(self):
@@ -171,18 +127,13 @@ class NM_BIDS:
         if PATH_NM_CHANNELS is not None:
             self.PATH_NM_CHANNELS = PATH_NM_CHANNELS
         # read nm_channels.csv or create nm_channels if None specified
-        self.settings_wrapper.set_nm_channels(
-            nm_channels_path=self.PATH_NM_CHANNELS,
-            ch_names=self.raw_arr.ch_names,
-            ch_types=self.raw_arr.get_channel_types(),
-            bads=self.raw_arr.info["bads"],
-            ECOG_ONLY=self.ECOG_ONLY,
-        )
+        self.settings_wrapper.set_nm_channels(nm_channels_path=self.PATH_NM_CHANNELS, ch_names=self.raw_arr.ch_names,
+                                              ch_types=self.raw_arr.get_channel_types(), bads=self.raw_arr.info["bads"],
+                                              ECOG_ONLY=self.ECOG_ONLY)
         self.settings_wrapper.set_fs_line_noise(self.fs, self.line_noise)
 
     def limit_data(self, LIMIT_LOW=0, LIMIT_HIGH=120000):
         """Reduce timing for faster test completion
-
         Parameters
         ----------
         LIMIT_LOW : int, optional
@@ -198,53 +149,39 @@ class NM_BIDS:
     def set_generator(self):
         """initialize generator for run function
         """
-        self.gen = nm_generator.ieeg_raw_generator(
-            self.raw_arr_data, self.settings_wrapper.settings
-        )
+        self.gen = nm_generator.ieeg_raw_generator(self.raw_arr_data, self.settings_wrapper.settings)
 
     def set_rereferencing(self):
         """initialize rereferencing
         """
         if self.settings_wrapper.settings["methods"]["re_referencing"] is True:
             self.rereference = nm_rereference.RT_rereference(
-                self.settings_wrapper.nm_channels, split_data=False
-            )
+                self.settings_wrapper.nm_channels, split_data=False)
         else:
             self.rereference = None
             # reset nm_channels from default values
             self.settings_wrapper.nm_channels["rereference"] = None
-            self.settings_wrapper.nm_channels[
-                "new_name"
-            ] = self.settings_wrapper.nm_channels["name"]
+            self.settings_wrapper.nm_channels["new_name"] = self.settings_wrapper.nm_channels["name"]
 
     def set_resampling(self):
         """define resampler for faster feature estimation
         """
         if self.settings_wrapper.settings["methods"]["raw_resampling"] is True:
-            self.resample = nm_resample.Resample(
-                self.settings_wrapper.settings
-            )
+            self.resample = nm_resample.Resample(self.settings_wrapper.settings)
         else:
             self.resample = None
 
     def set_features(self):
         """initialize feature class from settings
         """
-        self.features = nm_features.Features(
-            self.settings_wrapper.settings, verbose=self.verbose
-        )
+        self.features = nm_features.Features(self.settings_wrapper.settings, verbose=self.verbose)
 
     def set_run(self):
         """initialize run object
         """
         self.run_analysis = nm_run_analysis.Run(
-            self.features,
-            self.settings_wrapper.settings,
-            self.rereference,
-            self.projection,
-            self.resample,
-            verbose=self.verbose,
-        )
+            self.features, self.settings_wrapper.settings, self.rereference, self.projection,
+            self.resample, verbose=self.verbose)
 
     def call_run(self):
         while True:
@@ -258,22 +195,13 @@ class NM_BIDS:
         """add resampled labels to feature dataframe
         """
         self.df_features = nm_IO.add_labels(
-            self.run_analysis.feature_arr,
-            self.settings_wrapper,
-            self.raw_arr_data,
-        )
+            self.run_analysis.feature_arr, self.settings_wrapper, self.raw_arr_data)
 
     def save_features(self):
         """save settings.json, nm_channels.csv and features.csv
            and pickled run_analysis including projections
         """
-        self.run_analysis.feature_arr = (
-            self.df_features
-        )  # here the potential label stream is added
-        nm_IO.save_features_and_settings(
-            df_=self.df_features,
-            run_analysis=self.run_analysis,
-            folder_name=os.path.basename(self.PATH_RUN)[:-5],
-            settings_wrapper=self.settings_wrapper,
-        )
-
+        self.run_analysis.feature_arr = self.df_features  # here the potential label stream is added
+        nm_IO.save_features_and_settings(df_=self.df_features, run_analysis=self.run_analysis,
+                                         folder_name=os.path.basename(self.PATH_RUN)[:-5],
+                                         settings_wrapper=self.settings_wrapper)
