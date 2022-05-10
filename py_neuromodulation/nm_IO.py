fa0bf53cb4f9290f2ae3d50e5038e6400d4ef674
timonmerk <timon.merk@charite.de>
2022-05-10 21:14:17 +0200

    add mat file cast into dictionary
    add _OfflineStream Stream example

--- py_neuromodulation/nm_IO.py
+++ py_neuromodulation/nm_IO.py
@@ -319,3 +319,39 @@ def get_run_list_indir(PATH: str) -> list:
             if "FEATURES" in x:
                 f_files.append(os.path.basename(dirpath))
     return f_files
+
+
+def loadmat(filename):
+    """
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    """
+    data = io.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+
+def _check_keys(dict):
+    """
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    """
+    for key in dict:
+        if isinstance(dict[key], io.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+
+def _todict(matobj):
+    """
+    A recursive function which constructs from matobjects nested dictionaries
+    """
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, io.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
