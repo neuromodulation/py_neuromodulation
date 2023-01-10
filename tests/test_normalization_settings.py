22fd66f861b1437c9644bce6856ff95a2e5976ed
timonmerk <timon.merk@charite.de>
2023-01-10 17:45:36 +0100

    isolate norm settings

--- tests/test_normalization_settings.py
+++ tests/test_normalization_settings.py
@@ -1,163 +1,133 @@
 import os
 import unittest
+import pytest
+import numpy as np
 
-import py_neuromodulation as nm
-from py_neuromodulation import nm_define_nmchannels, nm_IO, nm_settings
+from py_neuromodulation import nm_normalization
 
+def test_raw_normalization_init():
 
-class TestNormSettings(unittest.TestCase):
-    def setUp(self) -> None:
-        """
-        Load BIDS data, initialize variables for testing different settings.
-        """
-        SCRIPT_DIR = os.path.join(os.path.abspath("."), "examples")
-        sub = "000"
-        ses = "right"
-        task = "force"
-        run = 3
-        datatype = "ieeg"
-
-        # Define run name and access paths in the BIDS format.
-        self.RUN_NAME = (
-            f"sub-{sub}_ses-{ses}_task-{task}_run-{run}_{datatype}.vhdr"
+    with pytest.raises(Exception):
+        nm_normalization.RawNormalizer(
+            sfreq=1000,
+            sampling_rate_features_hz=500,
+            normalization_method="meann",
+            normalization_time_s=30,
+            clip=3
         )
 
-        PATH_RUN = os.path.join(
-            (os.path.join(SCRIPT_DIR, "data")),
-            f"sub-{sub}",
-            f"ses-{ses}",
-            datatype,
-            self.RUN_NAME,
-        )
-        PATH_BIDS = os.path.join(SCRIPT_DIR, "data")
+def test_feature_normalization_init():
 
-        (
-            raw,
-            self.data,
-            self.sfreq,
-            self.line_noise,
-            self.coord_list,
-            self.coord_names,
-        ) = nm_IO.read_BIDS_data(
-            PATH_RUN=PATH_RUN, BIDS_PATH=PATH_BIDS, datatype=datatype
-        )
-        self.data = self.data[: int(5 * self.sfreq)]
-        self.settings = nm_settings.get_default_settings()
-
-        # Provide a path for the output data. Each re-referencing method has their PATH_OUT
-        self.PATH_OUT = os.path.join(
-            SCRIPT_DIR, "data", "derivatives", "test_normalization"
-        )
-        self.nm_channels = nm_define_nmchannels.set_channels(
-            ch_names=raw.ch_names,
-            ch_types=raw.get_channel_types(),
-            reference="default",
-            bads=raw.info["bads"],
-            new_names="default",
-            used_types=("ecog",),  # We focus only on LFP data
-            target_keywords=("SQUARED_ROTATION",),
+    with pytest.raises(Exception):
+        nm_normalization.FeatureNormalizer(
+            sampling_rate_features_hz=500,
+            normalization_method="meann",
+            normalization_time_s=30,
+            clip=3
         )
 
-    def test_fast_compute_settings(self) -> None:
-        """
-        Try if normalization on fast compute settings works.
-        No assertion in the end, only want to see if it raises any errors
-        """
-        self.setUp()
-        settings = nm_settings.set_settings_fast_compute(self.settings.copy())
-        stream = nm.Stream(
-            sfreq=self.sfreq,
-            settings=settings,
-            line_noise=self.line_noise,
-            nm_channels=self.nm_channels,
-            coord_names=self.coord_names,
-            coord_list=self.coord_list,
-            path_grids=None,
-            verbose=False,
-        )
-        stream.run(
-            data=self.data,
-            out_path_root=self.PATH_OUT,
-            folder_name=self.RUN_NAME,
+def test_process_norm_features():
+
+    norm = nm_normalization.FeatureNormalizer(
+        sampling_rate_features_hz=500,
+        normalization_method="mean",
+        normalization_time_s=30,
+        clip=3
+    )
+    data = np.ones([1, 5])
+    data_normed = norm.process(data)
+
+    assert np.all(np.isfinite(data_normed) == True)
+
+    assert np.all(np.equal(data, norm.previous) == 1)
+
+def test_previous_size_FeatureNorm():
+
+    norm = nm_normalization.FeatureNormalizer(
+        sampling_rate_features_hz=10,
+        normalization_method="zscore",
+        normalization_time_s=10,
+        clip=3
+    )
+
+    num_features = 5
+
+    for _ in range(150):
+        data = norm.process(np.random.random([1, num_features]))
+
+    assert norm.previous.shape[0] < norm.num_samples_normalize
+    
+def test_zscore_feature_analysis():    
+    norm = nm_normalization.FeatureNormalizer(
+        sampling_rate_features_hz=10,
+        normalization_method="zscore",
+        normalization_time_s=30,
+        clip=False
+    )
+
+    num_features = 5
+
+    for _ in range(400):
+        data_to_norm = np.random.random([1, num_features])
+        data_normed = norm.process(data_to_norm)
+
+    expect_res = norm.previous[:, 0].std() * data_normed[0, 0] + norm.previous[:, 0].mean()
+
+    assert pytest.approx(expect_res, 0.1) == data_to_norm[0, 0]
+
+def test_zscore_raw_analysis():
+
+    norm = nm_normalization.RawNormalizer(
+        sampling_rate_features_hz=10,
+        normalization_method="zscore",
+        normalization_time_s=30,
+        sfreq=10,
+        clip=False
+    )
+
+    num_samples = 100
+
+    for _ in range(400):
+        data_to_norm = np.random.random([1, num_samples])
+        data_normed = norm.process(data_to_norm)
+
+    expect_res = norm.previous[:, 0].std() * data_normed[0, 0] + norm.previous[:, 0].mean()
+
+    np.testing.assert_allclose(expect_res, data_to_norm[0, 0], rtol=0.1, atol=0.1)
+
+def test_all_norm_methods_raw():
+
+    for norm_method in [e.value for e in nm_normalization.NORM_METHODS]:
+        norm = nm_normalization.RawNormalizer(
+            sampling_rate_features_hz=10,
+            normalization_method=norm_method,
+            normalization_time_s=30,
+            sfreq=10,
+            clip=False
         )
 
-    def test_quantile_norm(self) -> None:
-        self.setUp()
-        settings = nm_settings.set_settings_fast_compute(self.settings.copy())
-        settings["preprocessing"] = ["raw_normalization"]
-        settings["postprocessing"]["feature_normalization"] = True
-        settings["raw_normalization_settings"][
-            "normalization_method"
-        ] = "quantile"
-        settings["feature_normalization_settings"][
-            "normalization_method"
-        ] = "quantile"
-        stream = nm.Stream(
-            sfreq=self.sfreq,
-            settings=settings,
-            line_noise=self.line_noise,
-            nm_channels=self.nm_channels,
-            coord_names=self.coord_names,
-            coord_list=self.coord_list,
-            path_grids=None,
-            verbose=False,
-        )
-        stream.run(
-            data=self.data,
-            out_path_root=self.PATH_OUT,
-            folder_name=self.RUN_NAME,
+        num_samples = 10
+
+        for _ in range(10):
+            data_to_norm = np.random.random([1, num_samples])
+            data_normed = norm.process(data_to_norm)
+
+        assert np.all(np.isfinite(data_normed) == True)
+
+def test_all_norm_methods_feature():
+
+    for norm_method in [e.value for e in nm_normalization.NORM_METHODS]:
+        norm = nm_normalization.FeatureNormalizer(
+            sampling_rate_features_hz=10,
+            normalization_method=norm_method,
+            normalization_time_s=30,
+            clip=False
         )
 
-    def test_zscore_median_norm(self) -> None:        
-        self.setUp()
-        settings = nm_settings.set_settings_fast_compute(self.settings.copy())
-        settings["preprocessing"] = ["raw_normalization"]
-        settings["postprocessing"]["feature_normalization"] = True
-        settings["raw_normalization_settings"][
-            "normalization_method"
-        ] = "zscore-median"
-        settings["feature_normalization_settings"][
-            "normalization_method"
-        ] = "zscore-median"
-        stream = nm.Stream(
-            sfreq=self.sfreq,
-            settings=settings,
-            line_noise=self.line_noise,
-            nm_channels=self.nm_channels,
-            coord_names=self.coord_names,
-            coord_list=self.coord_list,
-            path_grids=None,
-            verbose=False,
-        )
-        stream.run(
-            data=self.data,
-            out_path_root=self.PATH_OUT,
-            folder_name=self.RUN_NAME,
-        )
+        num_samples = 10
 
-    def test_minmax_norm(self) -> None:
-        self.setUp()
-        settings = nm_settings.set_settings_fast_compute(self.settings.copy())
-        settings["preprocessing"] = ["raw_normalization"]
-        settings["postprocessing"]["feature_normalization"] = True
-        settings["raw_normalization_settings"][
-            "normalization_method"
-        ] = "minmax"
-        settings["feature_normalization_settings"][
-            "normalization_method"
-        ] = "minmax"
-        stream = nm.Stream(
-            sfreq=self.sfreq,
-            settings=settings,
-            line_noise=self.line_noise,
-            nm_channels=self.nm_channels,
-            coord_names=self.coord_names,
-            coord_list=self.coord_list,
-            path_grids=None,
-            verbose=False,
-        )
-        stream.run(
-            data=self.data,
-            out_path_root=self.PATH_OUT,
-            folder_name=self.RUN_NAME,
-        )
+        for _ in range(10):
+            data_to_norm = np.random.random([1, num_samples])
+            data_normed = norm.process(data_to_norm)
+
+        assert np.all(np.isfinite(data_normed) == True)
