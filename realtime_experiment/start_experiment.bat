e6b8aa4dff964ab185a073cb31a9062024359b47
jlbusch <johannesleonbusch@gmail.com>
2023-01-23 15:13:33 +0100

    running batch file

--- realtime_experiment/start_experiment.bat
+++ realtime_experiment/start_experiment.bat
@@ -3,14 +3,15 @@ call conda activate realtime_decoding
 @REM #     the batch file will wait until the training program finishes
 @REM # Adding "start" opens it in a new window, and processes the next line
 @REM #     without waiting for the program to finish running
-call python "get_experiment_settings.py"
+@REM call python "get_experiment_settings.py"
 start python "start_decoding.py"
 ECHO Running realtime decoding
-TIMEOUT /T 5
+TIMEOUT /T 10
 start timeflux "timeflux_decoding.yaml"
 ECHO Running timeflux
 TIMEOUT /T 5
 START http://localhost:8000/monitor/
+TIMEOUT /T 5
 call conda activate bsl
 start bsl_stream_viewer -s SAGA
 
