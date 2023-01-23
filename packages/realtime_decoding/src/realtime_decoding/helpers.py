b6f2c86f1912f1234da3d0d0bac6f750d7b68fca
Richard Koehler <richard.koehler@outlook.de>
2023-01-23 08:45:14 +0100

    Update

--- packages/realtime_decoding/src/realtime_decoding/helpers.py
+++ packages/realtime_decoding/src/realtime_decoding/helpers.py
@@ -1,3 +1,15 @@
 import os
+import queue
 
 _PathLike = str | os.PathLike
+
+
+def clear_queue(q) -> None:
+    print("Emptying queue.")
+    try:
+        while True:
+            q.get(block=False)
+    except queue.Empty:
+        print("Queue emptied.")
+    except ValueError:  # Queue is already closed
+        print("Queue was already closed.")
