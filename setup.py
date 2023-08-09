42fb79e35cb10467f303ce23c6353ad2cdd50b90
timonmerk <timon.merk@charite.de>
2023-08-09 12:02:16 +0200

    add setup.py for rtd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="py_neuromodulation",
+    version="0.0.1dev",
+    package_dir={"": "py_neuromodulation"},
+    packages=[
+        "py_neuromodulation",
+    ],
+)
