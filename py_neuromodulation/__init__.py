61a900d9036c7419e3aebb33461b3acbeff048dc
Antonio Martinez Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-04-24 14:13:36 +0200

    fix logger circular import

--- py_neuromodulation/__init__.py
+++ py_neuromodulation/__init__.py
@@ -1,7 +1,7 @@
+# logger initialization first to prevent circular import
+from .utils import _logging  
+logger = _logging.logger
+
 # Bring Stream and DataProcessor classes to top namespace
 from .nm_stream_offline import Stream
 from .nm_run_analysis import DataProcessor
-
-__version__ = "0.1.0.dev1"
-
-from .utils import _logging  # logger initialization
