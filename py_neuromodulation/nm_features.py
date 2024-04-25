c757296e9b85ebf695f650be122c1836e63cf9ad
Antonio Martinez Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-04-25 13:23:04 +0200

    Change Feature to Protocol, lazy load feature modules

--- py_neuromodulation/nm_features.py
+++ py_neuromodulation/nm_features.py
@@ -1,24 +1,59 @@
+from importlib import import_module
+from typing import Protocol
+from collections.abc import Iterable
 import numpy as np
 
-from py_neuromodulation import (
-    nm_hjorth_raw,
-    nm_mne_connectivity,
-    nm_sharpwaves,
-    nm_coherence,
-    nm_fooof,
-    nm_nolds,
-    nm_features_abc,
-    nm_oscillatory,
-    nm_bursts,
-    nm_linelength,
-    nm_bispectra,
-)
 
+class NMFeature(Protocol):
+    def __init__(
+        self, settings: dict, ch_names: Iterable[str], sfreq: int | float
+    ) -> None:
+        """Method to check passed settings"""
 
+    @staticmethod
+    def test_settings(
+        settings: dict,
+        ch_names: Iterable[str],
+        sfreq: int | float,
+    ):
+        """Method to check passed settings"""
+
+    def calc_feature(self, data: np.ndarray, features_compute: dict) -> dict:
+        """
+        Feature calculation method. Each method needs to loop through all channels
+
+        Parameters
+        ----------
+        data : np.ndarray
+            (channels, time)
+        features_compute : dict
+        ch_names : Iterable[str]
+
+        Returns
+        -------
+        dict
+        """
+
+FEATURE_DICT : dict[str, tuple[str,str]] = {
+    "raw_hjorth": ("py_neuromodulation.nm_hjorth_raw", "Hjorth") ,
+    "return_raw": ("py_neuromodulation.nm_hjorth_raw", "Raw") ,
+    "bandpass_filter": ("py_neuromodulation.nm_oscillatory", "BandPower") ,
+    "stft": ("py_neuromodulation.nm_oscillatory", "STFT") ,
+    "fft": ("py_neuromodulation.nm_oscillatory", "FFT") ,
+    "welch": ("py_neuromodulation.nm_oscillatory", "Welch") ,
+    "sharpwave_analysis": ("py_neuromodulation.nm_sharpwaves", "SharpwaveAnalyzer") ,
+    "fooof": ("py_neuromodulation.nm_fooof", "FooofAnalyzer") ,
+    "nolds": ("py_neuromodulation.nm_nolds", "Nolds") ,
+    "coherence": ("py_neuromodulation.nm_coherence", "NM_Coherence") ,
+    "bursts": ("py_neuromodulation.nm_bursts", "Burst") ,
+    "linelength": ("py_neuromodulation.nm_linelength", "LineLength") ,
+    "mne_connectivity": ("py_neuromodulation.nm_mne_connectivity", "MNEConnectivity") ,
+    "bispectrum": ("py_neuromodulation.nm_bispectra", "Bispectra")
+}
+     
+             
 class Features:
-    """Class for calculating features."""
-
-    features: list[nm_features_abc.Feature] = []
+    """Class for calculating features.p"""
 
     def __init__(
         self, s: dict, ch_names: list[str], sfreq: int | float
@@ -40,48 +75,21 @@ class Features:
             _description_
         """
 
-        self.features = []
+        self.features: list[NMFeature]  = []
+        
+        for feature_name, feature_enabled in s["features"].items():
+            if feature_name not in FEATURE_DICT:
+                raise ValueError(f"Unknown feature found. Got: {feature_name}.")
 
-        for feature in s["features"]:
-            if s["features"][feature] is False:
-                continue
-            match feature:
-                case "raw_hjorth":
-                    FeatureClass = nm_hjorth_raw.Hjorth
-                case "return_raw":
-                    FeatureClass = nm_hjorth_raw.Raw
-                case "bandpass_filter":
-                    FeatureClass = nm_oscillatory.BandPower
-                case "stft":
-                    FeatureClass = nm_oscillatory.STFT
-                case "fft":
-                    FeatureClass = nm_oscillatory.FFT
-                case "welch":
-                    FeatureClass = nm_oscillatory.Welch
-                case "sharpwave_analysis":
-                    FeatureClass = nm_sharpwaves.SharpwaveAnalyzer
-                case "fooof":
-                    FeatureClass = nm_fooof.FooofAnalyzer
-                case "nolds":
-                    FeatureClass = nm_nolds.Nolds
-                case "coherence":
-                    FeatureClass = nm_coherence.NM_Coherence
-                case "bursts":
-                    FeatureClass = nm_bursts.Burst
-                case "linelength":
-                    FeatureClass = nm_linelength.LineLength
-                case "mne_connectivity":
-                    FeatureClass = nm_mne_connectivity.MNEConnectivity
-                case "bispectrum":
-                    FeatureClass = nm_bispectra.Bispectra
-                case _:
-                    raise ValueError(f"Unknown feature found. Got: {feature}.")
+            if feature_enabled:
+                feature_module = import_module(FEATURE_DICT[feature_name][0])
+                feature_classobj = getattr(feature_module, FEATURE_DICT[feature_name][1])
+                feature_classobj.test_settings(s, ch_names, sfreq)
+                feature_instance = feature_classobj(s, ch_names, sfreq)
+                self.features.append(feature_instance)
 
-            FeatureClass.test_settings(s, ch_names, sfreq)
-            f_obj = FeatureClass(s, ch_names, sfreq)
-            self.features.append(f_obj)
 
-    def register_new_feature(self, feature: nm_features_abc.Feature) -> None:
+    def register_new_feature(self, feature: NMFeature) -> None:
         """Register new feature.
 
         Parameters
