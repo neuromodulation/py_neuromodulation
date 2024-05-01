b014949a7f578298d52186f5f387a1d081494d1c
Antonio Martinez Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-05-02 00:43:00 +0200

    matplotlib: use tk backend

--- py_neuromodulation/__init__.py
+++ py_neuromodulation/__init__.py
@@ -1,11 +1,16 @@
 from .nm_logger import NMLogger
 from pathlib import PurePath
 from importlib.metadata import version
+import matplotlib
+
+# Set matplotlib backend to TkAgg because it crashes with the Qt5 install we're using for LSL
+matplotlib.use("tkagg")
 
 __version__ = version("py_neuromodulation")
 
 # Define constant for py_nm directory
 PYNM_DIR = PurePath(__file__).parent
+
 
 # logger initialization first to prevent circular import
 logger = NMLogger(__name__)
