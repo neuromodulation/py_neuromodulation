5b421f4410a8cc157d5f0617d9e30e209f4d9b99
Samed Rouven Vossberg <57455873+SamedVossberg@users.noreply.github.com>
2024-06-06 16:12:08 +0200

    add example, clean up nm_train

--- py_neuromodulation/nm_train.py
+++ py_neuromodulation/nm_train.py
@@ -3,64 +3,30 @@ from PySide6.QtCore import Signal, QThread
 from py_neuromodulation import (nm_settings, nm_mnelsl_stream, nm_define_nmchannels, nm_stream_offline)
 from mne_lsl.lsl import resolve_streams
 import pandas as pd
-from mne_lsl.stream import StreamLSL
-import time
-
-possible_streams = resolve_streams()
-possible_streams
-
-exg_stream = possible_streams[0]
-print(f'channel names: {exg_stream.get_channel_names()}')
-print(exg_stream.get_channel_info)
-
-
-settings = nm_settings.get_default_settings()
-settings["features"]["welch"] = False
-settings["features"]["fft"] = True
-settings["features"]["bursts"] = False
-settings["features"]["sharpwave_analysis"] = False
-settings["features"]["coherence"] = False
-
-
-# create an array ch_names with the channel names "ch0", "ch1", "ch2", ...
-ch_names = []
-ch_types = []
-for i in range(exg_stream.n_channels):
-    ch_names.append(f'ch{i}')
-    ch_types.append(exg_stream.stype)
-
-nm_channels = nm_define_nmchannels.set_channels(
-    ch_names = ch_names,
-    ch_types= ch_types,
-    reference = "default",
-    new_names = "default",
-    used_types= "eeg"
-)
-
-stream_name = exg_stream.name
-
-classes = ['relax', 'clench']
-stream = nm_stream_offline.Stream(sfreq=exg_stream.sfreq, nm_channels=nm_channels, settings=settings, verbose=True, line_noise=50)
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 class StreamWorker(QThread):
     update_signal = Signal(object)
 
-    def __init__(self, stream):
+    def __init__(self, stream, stream_name):
         super().__init__()
         self.stream = stream
+        self.stream_name = stream_name
 
     def run(self):
         print("Running")
-        print(stream_name)
         # stream = nm_mnelsl_stream.LSLStream(stream_name=stream_name, settings=settings)
-        data = stream.run(stream_lsl_name=stream_name, stream_lsl=True)
+        data = self.stream.run(stream_lsl_name = self.stream_name, stream_lsl=True)
         print(data)
         print("Data received")
-        
-class MainWindow(QWidget):
-    def __init__(self):
+
+class Trainer(QWidget):
+    def __init__(self, stream, stream_name):
         super().__init__()
         self.init_ui()
+        self.stream = stream
+        self.stream_name = stream_name
 
     def init_ui(self):
         self.layout = QVBoxLayout()
@@ -77,7 +43,7 @@ class MainWindow(QWidget):
 
     def start_thread(self):
         self.label.setText("Training started")
-        self.thread = StreamWorker(stream)
+        self.thread = StreamWorker(self.stream, self.stream_name)
         self.thread.update_signal.connect(self.update_label)
         self.thread.start()
 
@@ -91,13 +57,22 @@ class MainWindow(QWidget):
     def update_label(self, value):
         self.label.setText(f"Count: {value}")
 
-    def start():
-        app = QApplication([])
-        window = MainWindow()
+
+    # def train_model(self):
+
+    #     df_comb = pd.concat([df_relax, df_clench], ignore_index=True)
+
+    #     df_filtered = df_comb.drop(columns=['clench', 'time'])
+    #     features = df_filtered.columns[~df_filtered.columns.str.endswith('LineLength')]
+
+    #     X = df_comb[features]
+    #     y = df_comb['clench']
+
+    #     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    def start(self):
+        window = Trainer(self.stream, self.stream_name)
         window.show()
         app.exec()
 
 app = QApplication([])
-window = MainWindow()
-window.show()
-app.exec()
