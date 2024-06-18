a0e09b0f983b35bccb3b21dec281fc831b9590ec
Toni M. Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-06-18 10:32:47 +0200

    Optimize Hjorth and LineLength calculation (#340)
    * Optimize Hjorth calculation
    
    * Optimize LineLength
    
    * Update nm_hjorth_raw.py
    
    Adapt reference
    
    ---------
    
    Co-authored-by: timonmerk <timon.merk@charite.de>

--- py_neuromodulation/nm_hjorth_raw.py
+++ py_neuromodulation/nm_hjorth_raw.py
@@ -1,3 +1,10 @@
+"""
+Reference:  B Hjorth
+            EEG analysis based on time domain properties
+            Electroencephalogr Clin Neurophysiol. 1970 Sep;29(3):306-10.
+            DOI: 10.1016/0013-4694(70)90143-4
+"""
+
 import numpy as np
 from collections.abc import Iterable
 
@@ -6,23 +13,27 @@ from py_neuromodulation.nm_settings import NMSettings
 
 
 class Hjorth(NMFeature):
-    def __init__(self, settings: NMSettings, ch_names: Iterable[str], sfreq: float) -> None:
+    def __init__(
+        self, settings: NMSettings, ch_names: Iterable[str], sfreq: float
+    ) -> None:
         self.ch_names = ch_names
 
     def calc_feature(self, data: np.ndarray, features_compute: dict) -> dict:
-        for ch_idx, ch_name in enumerate(self.ch_names):
-            features_compute["_".join([ch_name, "RawHjorth_Activity"])] = np.nan_to_num(
-                np.var(data[ch_idx, :])
-            )
-            deriv_variance = np.nan_to_num(np.var(np.diff(data[ch_idx, :])))
-            mobility = np.nan_to_num(np.sqrt(deriv_variance / np.var(data[ch_idx, :])))
-            features_compute["_".join([ch_name, "RawHjorth_Mobility"])] = mobility
+        var = np.var(data, axis=-1)
+        deriv1 = np.diff(data, axis=-1)
+        deriv2 = np.diff(deriv1, axis=-1)
+        deriv1_var = np.var(deriv1, axis=-1)
+        deriv2_var = np.var(deriv2, axis=-1)
+        deriv1_mobility = np.sqrt(deriv2_var / deriv1_var)
 
-            dat_deriv_2_var = np.nan_to_num(np.var(np.diff(np.diff(data[ch_idx, :]))))
-            deriv_mobility = np.nan_to_num(np.sqrt(dat_deriv_2_var / deriv_variance))
-            features_compute["_".join([ch_name, "RawHjorth_Complexity"])] = (
-                np.nan_to_num(deriv_mobility / mobility)
-            )
+        activity = np.nan_to_num(var)
+        mobility = np.nan_to_num(np.sqrt(deriv1_var / var))
+        complexity = np.nan_to_num(deriv1_mobility / mobility)
+
+        for ch_idx, ch_name in enumerate(self.ch_names):
+            features_compute[f"{ch_name}_RawHjorth_Activity"] = activity[ch_idx]
+            features_compute[f"{ch_name}_RawHjorth_Mobility"] = mobility[ch_idx]
+            features_compute[f"{ch_name}_RawHjorth_Complexity"] = complexity[ch_idx]
 
         return features_compute
 
