7d3b48e67ee5a6ef8b139cf28e6140cbfe0a4135
elizametno <elizametno@yandex.ru>
2024-07-29 14:16:02 +0100

    Add Accordion component

---
+++ gui_dev/src/components/Accordion/AccordionData.js
@@ -0,0 +1,10 @@
+
+import { React } from 'react'; 
+import { Settings } from '@/components/Settings/Settings.js';
+
+const data = [ 
+    {title: 'Features',
+    component: <Settings />
+    }
+]
+export default data 
