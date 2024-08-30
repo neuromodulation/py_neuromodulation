ced0bad3e14f029cfedba6ded07e466602efad6a
Toni M. Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-08-30 04:43:11 +0200

    Fix about modal

--- gui_dev/src/components/AppInfoModal/AppInfoModal.jsx
+++ gui_dev/src/components/AppInfoModal/AppInfoModal.jsx
@@ -1,8 +1,30 @@
-import React from "react";
 import { Button } from "@mui/material";
 import { useAppInfo } from "@/stores/appInfoStore";
 import { SafeExternalLink as ExtLink } from "@/components/utils";
 import styles from "./AppInfoModal.module.css";
+
+// Auxiliary component defined outside
+const InfoItem = ({ label, value, isLink = false }) => {
+  const isEmpty =
+    value == null ||
+    value === "" ||
+    (Array.isArray(value) && value.length === 0);
+
+  if (isEmpty) {
+    return null; // Don't render anything if the value is empty
+  }
+
+  return (
+    <p>
+      <span className={styles.infoLabel}>{label}:</span>{" "}
+      {isLink ? (
+        <ExtLink href={value}>{value}</ExtLink>
+      ) : (
+        <span className={styles.infoValue}>{value}</span>
+      )}
+    </p>
+  );
+};
 
 export const AppInfoModal = ({ onClose }) => {
   const appInfo = useAppInfo();
@@ -10,26 +32,24 @@ export const AppInfoModal = ({ onClose }) => {
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <h2>App Information</h2>
+        <h2>About PyNeuromodulation</h2>
         <div className={styles.modalItems}>
-          <p>Version: {appInfo.version}</p>
-          <p>
-            Website: <ExtLink href={appInfo.website}>{appInfo.website}</ExtLink>
-          </p>
-          <p>Authors: {appInfo.authors.join(", ")}</p>
-          <p>Maintainers: {appInfo.maintainers.join(", ")}</p>
-          <p>
-            Repository:
-            <ExtLink href={appInfo.repository}>{appInfo.repository}</ExtLink>
-          </p>
-          <p>
-            Documentation:
-            <ExtLink href={appInfo.documentation}>
-              {appInfo.documentation}
-            </ExtLink>
-          </p>
-          <p>License: {appInfo.license}</p>
-          <p>Launch Mode: {appInfo.launchMode}</p>
+          <InfoItem label="Version" value={appInfo.version} />
+          <InfoItem label="Website" value={appInfo.website} isLink />
+          <InfoItem label="Authors" value={appInfo.authors.join(", ")} />
+          <InfoItem
+            label="Maintainers"
+            value={appInfo.maintainers.join(", ")}
+          />
+          <InfoItem label="Repository" value={appInfo.repository} isLink />
+          <InfoItem
+            label="Documentation"
+            value={appInfo.documentation}
+            isLink
+          />
+          <InfoItem label="License" value={appInfo.license} />
+          <InfoItem label="Launch Mode" value={appInfo.launchMode} />
+
           <Button variant="contained" onClick={onClose}>
             Close
           </Button>
