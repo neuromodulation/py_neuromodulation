d36d85579ac7d2b8bb63defaddce268202f6799c
Toni M. Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-09-11 13:12:54 +0200

    Add jsconfig and Eslint jsdoc plugin

--- gui_dev/eslint.config.js
+++ gui_dev/eslint.config.js
@@ -1,5 +1,6 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -7,20 +8,22 @@ import reactCompiler from "eslint-plugin-react-compiler";
 import babelParser from "@babel/eslint-parser";
 
 export default [
+  js.configs.recommended,
+  jsdoc.configs["flat/stylistic-typescript-flavor"],
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     plugins: {
+      jsdoc,
       react,
       reactHooks,
       "react-refresh": reactRefresh,
       "react-compiler": reactCompiler,
     },
     languageOptions: {
-      ecmaVersion: "latest", // or a specific version like 2021
+      ecmaVersion: "latest",
       sourceType: "module",
       parser: babelParser,
       parserOptions: {
