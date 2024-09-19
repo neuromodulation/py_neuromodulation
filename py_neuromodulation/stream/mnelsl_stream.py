66deeae334b053b3c8a5bac124a54a2846bc4ea6
Toni M. Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-09-19 10:58:58 +0200

    add custom keyboard listener, drop pynput dependency

--- py_neuromodulation/stream/mnelsl_stream.py
+++ py_neuromodulation/stream/mnelsl_stream.py
@@ -8,7 +8,7 @@ import os
 
 if TYPE_CHECKING:
     from py_neuromodulation import NMSettings
-    
+
 
 class LSLStream:
     """
@@ -32,8 +32,10 @@ class LSLStream:
             under the same name.
         """
         from mne_lsl.stream import StreamLSL
+
         self.stream: StreamLSL
-        
+        self.keyboard_interrupt = False
+
         self.settings = settings
         self._n_seconds_wait_before_disconnect = 3
         try:
@@ -50,18 +52,16 @@ class LSLStream:
 
         if self.stream.sinfo is None:
             raise RuntimeError("Stream info is None. Check if the stream is running.")
-        
+
         self.winsize = settings.segment_length_features_ms / self.stream.sinfo.sfreq
         self.sampling_interval = 1 / self.settings.sampling_rate_features_hz
 
         # If not running the generator when the escape key is pressed.
         self.headless: bool = not os.environ.get("DISPLAY")
         if not self.headless:
-            from pynput import keyboard
+            from py_neuromodulation.utils.keyboard import KeyboardListener
 
-            self.listener = keyboard.Listener(
-                on_press=lambda key: key != keyboard.Key.esc  # type: ignore
-            )
+            self.listener = KeyboardListener(("esc", self.set_keyboard_interrupt))
             self.listener.start()
 
     def get_next_batch(self) -> Iterator[tuple[np.ndarray, np.ndarray]]:
@@ -111,6 +111,10 @@ class LSLStream:
 
                 logger.info(f"Stream time: {timestamp[-1] - stream_start_time}")
 
-                if not self.headless and not self.listener.running:
+                if not self.headless and self.keyboard_interrupt:
                     logger.info("Keyboard interrupt")
+                    self.listener.stop()
                     self.stream.disconnect()
+
+    def set_keyboard_interrupt(self):
+        self.keyboard_interrupt = True
