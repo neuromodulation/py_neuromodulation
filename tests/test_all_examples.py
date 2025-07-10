9aaacbba4be1f24f5a8d44b833949422525df4a3
timonmerk <timon.merk95@gmail.com>
2025-07-10 17:18:21 -0500

    fix mac test

--- tests/test_all_examples.py
+++ tests/test_all_examples.py
@@ -4,7 +4,6 @@ import runpy
 
 
 @pytest.mark.parametrize("example_filename", Path("examples").glob("*.py"))
-@pytest.mark.no_parallel
 def test_run_through_all_test(example_filename):
     print(f"Running {example_filename}")
     if "plot_8_cebra_example.py" not in example_filename.name:
